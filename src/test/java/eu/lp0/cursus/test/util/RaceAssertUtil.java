b11b65dd164e86428523c9f1fd86938b9bf6147e
Simon Arlott <sa.me.uk>
2011-11-13 13:12:57 +0000

    check simulated race points have been applied to the correct pilots

--- src/test/java/eu/lp0/cursus/test/util/RaceAssertUtil.java
+++ src/test/java/eu/lp0/cursus/test/util/RaceAssertUtil.java
@@ -17,12 +17,16 @@
  */
 package eu.lp0.cursus.test.util;
 
+import java.util.Collection;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import org.junit.Assert;
+
+import com.google.common.collect.Sets;
 
 import eu.lp0.cursus.db.data.Pilot;
 import eu.lp0.cursus.db.data.Race;
@@ -32,6 +36,7 @@ public class RaceAssertUtil {
 	private final Map<Pilot, Integer> actualRaceLaps;
 	private final Map<Pilot, Integer> actualRacePenalties;
 	private final Map<Pilot, Integer> actualRacePoints;
+	private final Collection<Pilot> actualSimulatedRacePoints;
 	private final Map<Pilot, Integer> actualRacePositions;
 	private final List<Pilot> actualRaceOrder;
 
@@ -44,24 +49,32 @@ public class RaceAssertUtil {
 		actualRaceLaps = scores.getLaps(race);
 		actualRacePenalties = scores.getRacePenalties(race);
 		actualRacePoints = scores.getRacePoints(race);
+		actualSimulatedRacePoints = scores.getSimulatedRacePoints(race);
 		actualRacePositions = scores.getRacePositions(race);
 		actualRaceOrder = scores.getRaceOrder(race);
 	}
 
-	public void assertPilot(Pilot pilot, int expectedLaps, int expectedPenalties, int expectedPoints, int expectedPosition) {
+	public void assertPilot(Pilot pilot, int expectedLaps, int expectedPenalties, boolean expectedSimulatedRacePoints, int expectedPoints, int expectedPosition) {
 		Assert.assertFalse(done);
+
+		if (expectedSimulatedRacePoints) {
+			Assert.assertEquals("Pilot " + pilot.getName() + " cannot have simulated race points with non-zero laps", 0, expectedLaps); //$NON-NLS-1$//$NON-NLS-2$
+		}
 
 		Assert.assertTrue("Pilot " + pilot.getName() + " does not exist in scores", actualRacePoints.containsKey(pilot)); //$NON-NLS-1$//$NON-NLS-2$
 		Assert.assertEquals("Race laps mismatch for " + pilot.getName(), expectedLaps, (int)actualRaceLaps.get(pilot)); //$NON-NLS-1$
 		Assert.assertEquals("Race penalties mismatch for " + pilot.getName(), expectedPenalties, (int)actualRacePenalties.get(pilot)); //$NON-NLS-1$
+		Assert.assertEquals("Race points simulation mismatch for " + pilot.getName(), expectedSimulatedRacePoints, actualSimulatedRacePoints.contains(pilot)); //$NON-NLS-1$
 		Assert.assertEquals("Race points mismatch for " + pilot.getName(), expectedPoints, (int)actualRacePoints.get(pilot)); //$NON-NLS-1$
 		Assert.assertEquals("Race position mismatch for " + pilot.getName(), expectedPosition, (int)actualRacePositions.get(pilot)); //$NON-NLS-1$
 		Assert.assertTrue("Pilot " + pilot.getName() + " already specified", expectedRaceOrder.add(pilot)); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
-	public void assertOrder() {
+	public void assertDone(int expectedSimulatedRacePointsPilots) {
 		Assert.assertFalse(done);
 
+		Assert.assertArrayEquals(new Object[0], Sets.difference(new HashSet<Pilot>(actualSimulatedRacePoints), expectedRaceOrder).toArray());
+		Assert.assertEquals(expectedSimulatedRacePointsPilots, actualSimulatedRacePoints.size());
 		Assert.assertEquals(expectedPilots, expectedRaceOrder.size());
 		Assert.assertEquals(expectedPilots, actualRaceOrder.size());
 		Assert.assertArrayEquals(expectedRaceOrder.toArray(), actualRaceOrder.toArray());
