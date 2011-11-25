d799d5a424b00d43c08f7dedf5d5b12bf0264297
Simon Arlott <sa.me.uk>
2011-11-25 22:58:21 +0000

    always allow repaint on table cell JButtons

--- src/main/java/eu/lp0/cursus/ui/component/DeleteDatabaseColumnModel.java
+++ src/main/java/eu/lp0/cursus/ui/component/DeleteDatabaseColumnModel.java
@@ -20,7 +20,6 @@ package eu.lp0.cursus.ui.component;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Graphics;
-import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
@@ -318,20 +317,6 @@ public abstract class DeleteDatabaseColumnModel<T extends AbstractEntity> extend
 			} else {
 				setForeground(fg);
 				setBackground(bg);
-			}
-		}
-
-		@Override
-		public void repaint(long tm, int x, int y, int width, int height) {
-			if (focus) {
-				super.repaint(tm, x, y, width, height);
-			}
-		}
-
-		@Override
-		public void repaint(Rectangle r) {
-			if (focus) {
-				super.repaint(r);
 			}
 		}
 
