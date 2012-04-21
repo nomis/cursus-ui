516117829e169b5533c5da74e4a030aa2c5f413a
Simon Arlott <sa.me.uk>
2012-04-21 18:33:19 +0100

    update copyright years

--- src/main/java/eu/lp0/cursus/scoring/ScoringSystem.java
+++ src/main/java/eu/lp0/cursus/scoring/ScoringSystem.java
@@ -1,6 +1,6 @@
 /*
 	cursus - Race series management program
-	Copyright 2011  Simon Arlott
+	Copyright 2012  Simon Arlott
 
 	This program is free software: you can redistribute it and/or modify
 	it under the terms of the GNU General Public License as published by
